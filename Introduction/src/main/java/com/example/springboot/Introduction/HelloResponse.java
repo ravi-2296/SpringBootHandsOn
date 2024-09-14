89c59edf7af60e8f4737fccad98c1c2f46a740f6
Ravi Ranjan <ravikumarranjan654@gmail.com>
2024-09-15 01:07:30 +0530

    json response

---
+++ Introduction/src/main/java/com/example/springboot/Introduction/HelloResponse.java
@@ -0,0 +1,20 @@
+package com.example.springboot.Introduction;
+
+public class HelloResponse {
+
+    private String message;
+
+    public HelloResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+}
